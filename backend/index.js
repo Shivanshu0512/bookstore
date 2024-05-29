68f371f9f355ac23edfe8822e4356423e9a7027c
Shivanshu0512 <shivanshum512@gmail.com>
2024-05-30 02:04:03 +0530

    get one book

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,7 @@ app.post('/books', async(request, response)=> {
     }
 });
 
+//route for getting all books from database
 app.get('/books',async (request,response) => {
 try{
     const books = await Book.find({});
@@ -49,6 +50,19 @@ try{
 }
 
 });
+
+//route for get one book from database
+app.get('/books/:id',async (request,response) => {
+    try{
+        const { id } = request.params;
+        const book = await Book.findById({id});
+        return response.status(200).json(book);
+    } catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+    
+    });
 
 mongoose.connect(mongoDBURL)
 .then(()=>{
