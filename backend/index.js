3d43ee275af307fd2ae7ef03f936413d18d8cfcf
Shivanshu0512 <shivanshum512@gmail.com>
2024-05-30 03:32:14 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,17 @@ import express from "express";
 import { PORT,mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import{ Book } from './models/bookModels.js';
-import bookRoutes from '.routes/booksRoutes.js'
+import bookRoutes from './routes/booksRoutes.js';
+import cors from 'cors';
+
 const app= express();
 app.use(express.json());
+//middleware for handling CORS policy
+//option 1: allow all origins with defaultvof cors(*)
+//app.use(cors());
+//option 2: allow custom origins
+
+
 app.get('/',(request,response) => {
     console.log(request);
     return response.status(234).send('Heluuu');
